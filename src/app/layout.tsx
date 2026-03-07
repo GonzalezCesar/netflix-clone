df95849330ea7f55423d73f8e4e26fd3ba3fa619
GonzalezCesar <gonzalez.abachecesar@gmail.com>
2026-03-06 23:24:30 -0400

    feat: add new sections to home page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+// import { ClickToComponent } from "click-to-react-component";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
